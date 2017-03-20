e0ba119d1699484231910f4f2fb9e4942a44850a
Chris Sullivan <sullivan@nscl.msu.edu>
2017-03-20 16:59:59 -0400

    Small comment update to nulib.py

--- nulib.py
+++ nulib.py
@@ -1,5 +1,10 @@
-from PyNuLib import *
 import numpy as np
+from PyNuLib import *
+
+# This is a helper module meant to import the necessary PyNuLib code
+# and wrap functionality up into simply python functions and classes.
+# If you would like to expose additional NuLib internals (from the
+# f90 runtime), you can do so in src/pyinterface.F90
 
 def InitializeNuLib():
     parameters_filename = "./parameters"
@@ -8,9 +13,6 @@ def InitializeNuLib():
 
 def GetNuclei():
     species = []
-
-    #print(Pynulib.nuclei_a)
-
     for i, a in enumerate(pynulib.nuclei_a):
         species.append((a,pynulib.nuclei_z[i]))
     return species
